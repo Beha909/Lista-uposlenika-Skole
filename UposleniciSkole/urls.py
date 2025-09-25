e2392e17d6ea1017e816db7ecdd8c55eeaec5eb5
Adi Behaderović <behaderovicadi33@gmail.com>
2025-09-25 18:13:07 +0200

    Dodaj dva URLa, jedan za uposlenike drugi za ucenike

--- UposleniciSkole/urls.py
+++ UposleniciSkole/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.lista_uposlenika, name="lista_uposlenika"),
-    path("dodaj/", views.dodaj_uposlenika, name="dodaj_uposlenika"),
-    path("obrisi/<int:pk>/", views.obrisi_uposlenika, name="obrisi_uposlenika"),
+    path("uposlenici/", views.lista_objekata, {"tip": "uposlenici"}, name="lista_uposlenika"),
+    path("ucenici/", views.lista_objekata, {"tip": "ucenici"}, name="lista_ucenika"),
+    path("uposlenici/dodaj/", views.dodaj_uposlenika, name="dodaj_uposlenika"),
+    path("uposlenici/obrisi/<int:pk>/", views.obrisi_uposlenika, name="obrisi_uposlenika"),
 ]
