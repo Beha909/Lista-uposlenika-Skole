6ae5785ed9e7de5d9530dfa6b63d1df3edc256a1
Adi Behaderović <behaderovicadi33@gmail.com>
2025-09-25 18:07:18 +0200

    Dodaj novu formu za ucenike

--- UposleniciSkole/forms.py
+++ UposleniciSkole/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Uposlenik
+from .models import Uposlenik, Ucenik
 
 class UposlenikForm(forms.ModelForm):
     class Meta:
         model = Uposlenik
         fields = ["ime", "prezime", "pozicija", "godine_rada", "pravo_na_godisnji",]
+
+class UcenikForm(forms.ModelForm):
+    class Meta:
+        model = Ucenik
+        fields = ["ime", "prezime", "godiste"]
