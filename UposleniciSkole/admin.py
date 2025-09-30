777847337bf2b3d3ee04f8e6e86d78d9d0762123
Adi Behaderović <behaderovicadi33@gmail.com>
2025-09-30 18:10:16 +0200

    bug fix

--- UposleniciSkole/admin.py
+++ UposleniciSkole/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Uposlenik
+from .models import Uposlenik, Ucenik
 
 
 
 # Register your models here.
 
 admin.site.register(Uposlenik)
+admin.site.register(Ucenik)
 
 
 
